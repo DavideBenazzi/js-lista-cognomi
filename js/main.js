a505f92eb088b03250bd30b328f0173b7212ec3e
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-19 14:39:25 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+Lista Cognomi
+Chiedere all'utente il cognome
+inserirlo in un array con altri cognomi: 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*/
+var user = prompt('Qual\'è il tuo cognome?');
+
+var lista = ['Bianchi' , 'Rossi' , 'Duzioni' , 'Balsano' , 'Giorgini'];
