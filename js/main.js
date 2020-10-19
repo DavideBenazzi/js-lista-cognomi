22df3c2fa69e9e4543cce62dc2a86a11c7761697
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-19 17:41:59 +0200

    Modifiche Finali

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ stampa la lista ordinata alfabeticamente
 scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 */
 var user = prompt('Qual\'è il tuo cognome?').trim();
-
+// TRASFORMO LA PRIMA LETTERA IN MAIUSCOLA
 var cognome = user.charAt(0).toUpperCase() + user.slice(1).toLowerCase();
-console.log(cognome);
-
+// LISTA COGNOMI
 var lista = ['Bianchi' , 'Rossi' , 'Duzioni' , 'Balsano' , 'Giorgini'];
-
+// AGGIUNGO IL COGNOME ALLA LISTA
 lista.push(cognome);
-
-console.log( lista.sort() );
-console.log( lista.indexOf(cognome) + 1 );
+// LISTA COGNOMI AGGIORNATA
+document.getElementById('risultato').innerHTML = lista.sort();
+// POSIZIONE NELLA LISTA
+document.getElementById('posizione').innerHTML = 'Il tuo cognome si trova nella posizione ' + (lista.indexOf(cognome) + 1) + ' della lista.';
