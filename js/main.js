c13b00b8c8642fd23d2c3156882c91972330f81b
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-19 17:14:20 +0200

    ottimizzazione algoritmo

--- js/main.js
+++ js/main.js
@@ -6,16 +6,13 @@ stampa la lista ordinata alfabeticamente
 scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 */
 var user = prompt('Qual\'è il tuo cognome?').trim();
-primaLettera = user.charAt(0);
 
-maiuscolo = primaLettera.toUpperCase();
-
-nuovoUser = user.replace( primaLettera , maiuscolo );
-
+var cognome = user.charAt(0).toUpperCase() + user.slice(1).toLowerCase();
+console.log(cognome);
 
 var lista = ['Bianchi' , 'Rossi' , 'Duzioni' , 'Balsano' , 'Giorgini'];
 
-lista.push(nuovoUser);
+lista.push(cognome);
 
 console.log( lista.sort() );
-console.log( lista.indexOf(nuovoUser) + 1 );
+console.log( lista.indexOf(cognome) + 1 );
